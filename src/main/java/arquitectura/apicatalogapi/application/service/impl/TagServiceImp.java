7ded2f7b0b1f50db0b439e84c284902bad080012
Marcos <marcrazesp@outlook.es>
2023-09-16 13:37:21 +0200

    feat(TagDelete): Arreglado el bug que no dejaba eliminar el Tag

--- src/main/java/arquitectura/apicatalogapi/application/service/impl/TagServiceImp.java
+++ src/main/java/arquitectura/apicatalogapi/application/service/impl/TagServiceImp.java
@@ -17,69 +17,78 @@ import arquitectura.apicatalogapi.infrastructure.repository.TagRepository;
 @Service
 public class TagServiceImp implements TagService {
 
-    String message = "El ID no existe";
+	String message = "El ID no existe";
 
-    @Autowired
-    private TagRepository repository;
+	@Autowired
+	private TagRepository repository;
 
-    @Autowired
-    private EndpointRepository endpointRepository;
+	@Autowired
+	private EndpointRepository endpointRepository;
 
-    @Override
-    @Transactional(readOnly = true)
-    public List<Tag> getAllTags() {
-        return this.repository.findAll();
-    }
+	@Override
+	@Transactional(readOnly = true)
+	public List<Tag> getAllTags() {
+		return this.repository.findAll();
+	}
 
-    @Override
-    @Transactional(readOnly = true)
-    public Optional<Tag> getTagById(int id) {
-        Optional<Tag> entityExists = repository.findById(id);
-        if (entityExists.isPresent()) {
-            return this.repository.findById(id);
-        } else {
-            throw new IllegalArgumentException(message);
-        }
-    }
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<Tag> getTagById(int id) {
+		Optional<Tag> entityExists = repository.findById(id);
+		if (entityExists.isPresent()) {
+			return this.repository.findById(id);
+		} else {
+			throw new IllegalArgumentException(message);
+		}
+	}
 
-    @Override
-    @Transactional
-    public void delete(int id) {
-        Optional<Tag> entityExists = repository.findById(id);
-        if (entityExists.isPresent()) {
-            this.repository.deleteById(id);
-        } else {
-            throw new IllegalArgumentException(message);
-        }
-    }
+	@Override
+	@Transactional
+	public void delete(int id) {
+		Optional<Tag> entityExists = repository.findById(id);
+		if (entityExists.isPresent()) {
+			Tag tagToDelete = entityExists.get();
 
-    @Override
-    @Transactional
-    public Tag create(Tag tag, Integer idAplicacion, List<Integer> idEndpoints) {
-        tag.setAplicacion(new Aplicacion(idAplicacion));
-        List<Endpoint> endpoints = endpointRepository.findAllById(idEndpoints);
-        tag.setEndpoints(endpoints);
-        return this.repository.save(tag);
-    }
+			for (Endpoint endpoint : tagToDelete.getEndpoints()) {
+				endpoint.getTags().remove(tagToDelete);
+			}
+			tagToDelete.getEndpoints().clear();
 
-    @Override
-    @Transactional
-    public Tag create(Tag tag, Integer idAplicacion) {
-        tag.setAplicacion(new Aplicacion(idAplicacion));
-        return this.repository.save(tag);
-    }
+			tagToDelete.getAplicacion().getTags().remove(tagToDelete);
 
-    @Override
-    @Transactional
-    public Tag update(int id, Tag tag) {
-        tag.setId(id);
-        Optional<Tag> entityExists = repository.findById(id);
-        if (entityExists.isPresent()) {
-            Tag entity = entityExists.get();
-            entity.setNombre(tag.getNombre());
-            return repository.save(entity);
-        } else {
-            throw new IllegalArgumentException(message);
-        }
-    }
+			repository.delete(tagToDelete);
+		} else {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	@Override
+	@Transactional
+	public Tag create(Tag tag, Integer idAplicacion, List<Integer> idEndpoints) {
+		tag.setAplicacion(new Aplicacion(idAplicacion));
+		List<Endpoint> endpoints = endpointRepository.findAllById(idEndpoints);
+		tag.setEndpoints(endpoints);
+		return this.repository.save(tag);
+	}
+
+	@Override
+	@Transactional
+	public Tag create(Tag tag, Integer idAplicacion) {
+		tag.setAplicacion(new Aplicacion(idAplicacion));
+		return this.repository.save(tag);
+	}
+
+	@Override
+	@Transactional
+	public Tag update(int id, Tag tag) {
+		tag.setId(id);
+		Optional<Tag> entityExists = repository.findById(id);
+		if (entityExists.isPresent()) {
+			Tag entity = entityExists.get();
+			entity.setNombre(tag.getNombre());
+			return repository.save(entity);
+		} else {
+			throw new IllegalArgumentException(message);
+		}
+	}
 }
